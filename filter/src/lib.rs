c821909898492f261c0fd4ffdd893ad4540d01f6
jaxs-ribs <luc.baracat@googlemail.com>
2024-03-21 20:43:54 +0100

    WIP

--- filter/src/lib.rs
+++ filter/src/lib.rs
@@ -1,20 +1,20 @@
 use llm_types::openai::ChatParams;
 use rand::Rng;
 use serde::{Deserialize, Serialize};
-use std::str::FromStr;
 use serde_json::Value;
-
+use std::str::FromStr;
 
 use kinode_process_lib::{
-    await_message, call_init, println, Address, ProcessId, Request, Response, http, Message, get_blob 
+    await_message, call_init, get_blob, http, println, Address, Message, ProcessId, Request,
+    Response,
 };
 use std::collections::HashMap;
 
 mod groq_api;
 mod llm_types;
 
+use groq_api::{spawn_groq_pkg, GroqApi};
 use llm_types::openai::Message as GroqMessage;
-use groq_api::{GroqApi, spawn_groq_pkg};
 
 wit_bindgen::generate!({
     path: "wit",
@@ -26,6 +26,14 @@ wit_bindgen::generate!({
 
 const PROCESS_ID: &str = "filter:filter:template.os";
 const GROQ_KEY: &str = include_str!("../../pkg/.groq_key");
+
+// TODO: Zena: Is this OK? Look where it's being used.
+const DEFAULT_HEADERS: HashMap<&str, &str> = HashMap::from([
+    ("Content-Type", "application/json"),
+    ("Access-Control-Allow-Origin", "*"),
+    ("Access-Control-Allow-Headers", "Content-Type"),
+    ("Access-Control-Allow-Methods", "GET, POST, OPTIONS"),
+]);
 
 fn handle_internal_messages(our: &Address) -> anyhow::Result<()> {
     let message = await_message()?;
@@ -44,13 +52,8 @@ fn handle_http_messages(our: &Address, message: &Message) -> Option<()> {
             let http_request = server_request.request()?;
             if http_request.method().ok()? == http::Method::OPTIONS {
                 // Handle OPTIONS request by returning the necessary CORS headers
-                let headers = HashMap::from([
-                    ("Content-Type".to_string(), "application/json".to_string()),
-                    ("Access-Control-Allow-Origin".to_string(), "*".to_string()),
-                    ("Access-Control-Allow-Headers".to_string(), "Content-Type".to_string()),
-                    ("Access-Control-Allow-Methods".to_string(), "GET, POST, OPTIONS".to_string()),
-                ]);
-                let _ = http::send_response(http::StatusCode::OK, Some(headers), Vec::new());
+                let _ =
+                    http::send_response(http::StatusCode::OK, Some(DEFAULT_HEADERS), Vec::new());
                 return None;
             }
             let body = get_blob()?;
@@ -69,48 +72,56 @@ fn handle_http_messages(our: &Address, message: &Message) -> Option<()> {
     None
 }
 
+fn fetch_status(our: &Address) -> Option<()> {
+    let mut rng = rand::thread_rng();
+    let status = if rng.gen() { "AAA" } else { "BBB" };
+    let response = serde_json::to_string(&status).ok()?;
+    println!("sending response: {}", response);
+    let _ = http::send_response(
+        http::StatusCode::OK,
+        Some(DEFAULT_HEADERS),
+        response.as_bytes().to_vec(),
+    );
+    None
+}
+
 fn send_tweet(our: &Address, body: &[u8]) -> Option<()> {
-    let tweets: Vec<String> = match serde_json::from_slice::<serde_json::Value>(body).ok()?["tweets"].as_array() {
-        Some(tweets) => tweets.iter().filter_map(|tweet| tweet.as_str().map(String::from)).collect(),
-        None => vec![],
-    };
+    let tweets: Vec<String> =
+        match serde_json::from_slice::<serde_json::Value>(body).ok()?["tweets"].as_array() {
+            Some(tweets) => tweets
+                .iter()
+                .filter_map(|tweet| tweet.as_str().map(String::from))
+                .collect(),
+            None => vec![],
+        };
     let modified_tweets: Vec<String> = tweets.iter().map(|tweet| tweet.to_uppercase()).collect();
-    let response_body = serde_json::to_string(&serde_json::json!({ "tweets": modified_tweets })).ok()?;
-    // TODO: Zena: Is this OK? 
-    let headers = HashMap::from([
-        ("Content-Type".to_string(), "application/json".to_string()),
-        ("Access-Control-Allow-Origin".to_string(), "*".to_string()),
-        ("Access-Control-Allow-Headers".to_string(), "Content-Type".to_string()),
-        ("Access-Control-Allow-Methods".to_string(), "GET, POST, OPTIONS".to_string()),
-    ]);
+    let response_body =
+        serde_json::to_string(&serde_json::json!({ "tweets": modified_tweets })).ok()?;
     println!("sending tweets response: {}", response_body);
-    let _ = http::send_response(http::StatusCode::OK, Some(headers), response_body.as_bytes().to_vec());
+    let _ = http::send_response(
+        http::StatusCode::OK,
+        Some(DEFAULT_HEADERS),
+        response_body.as_bytes().to_vec(),
+    );
     None
 }
 
 // TODO: Zena: THe problem is that we're first getting a preflight request!
 
-fn fetch_status(our: &Address) -> Option<()> {
-    let mut rng = rand::thread_rng();
-    let status = if rng.gen() { "AAA" } else { "BBB" };
-    let response = serde_json::to_string(&status).ok()?;
-    // TODO: Zena: Is this OK? 
-    let headers = HashMap::from([
-        ("Content-Type".to_string(), "application/json".to_string()),
-        ("Access-Control-Allow-Origin".to_string(), "*".to_string()),
-        ("Access-Control-Allow-Headers".to_string(), "Content-Type".to_string()),
-        ("Access-Control-Allow-Methods".to_string(), "GET, POST, OPTIONS".to_string()),
-    ]);
-    println!("sending response: {}", response);
-    let _ = http::send_response(http::StatusCode::OK, Some(headers), response.as_bytes().to_vec());
-    None
-}
-
 fn make_request(our: &Address) -> anyhow::Result<()> {
     let api = spawn_groq_pkg(our, GROQ_KEY)?;
+    let content = r###"
+    You are a helpful assistant that helps to filter out tweets that don't adhere to certain rules. 
+    These rules are as follows: 
+    - Does not talk about a technical topic
+    - Talks about a memecoin
+    - Uses profanity
+    You will receive a series of tweets. For each of the tweets, you either answer y or n, with y meaning the tweet passes the aforementioned rules and n meaning it does not.
+    Don't answer with anything else, only y or n. Don't even use a space between anything, the entire output should just be a series of y's and n's.
+    "###;
     let system_prompt = GroqMessage {
         role: "system".into(),
-        content: "You are a helpful assistant.".into(),
+        content: content.into(),
     };
     let test_prompt = GroqMessage {
         role: "user".into(),
@@ -124,7 +135,7 @@ fn make_request(our: &Address) -> anyhow::Result<()> {
 
 fn create_chat_params(messages: Vec<GroqMessage>) -> ChatParams {
     let chat_params = ChatParams {
-        model: "mixtral-8x7b-32768".into(), 
+        model: "mixtral-8x7b-32768".into(),
         messages,
         max_tokens: Some(1200),
         temperature: Some(0.0),
@@ -132,7 +143,6 @@ fn create_chat_params(messages: Vec<GroqMessage>) -> ChatParams {
     };
     chat_params
 }
-
 
 call_init!(init);
 fn init(our: Address) {
