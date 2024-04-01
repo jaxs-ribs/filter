2f4037a865f811a15d8a38fab07a2294969b0b1c
jaxs-ribs <luc.baracat@googlemail.com>
2024-04-01 15:23:40 +0200

    Some cleanups

--- filter/src/lib.rs
+++ filter/src/lib.rs
@@ -1,16 +1,8 @@
-use rand::Rng;
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
-use std::str::FromStr;
-
-use kinode_process_lib::{
-    await_message, call_init, get_blob, http, println, Address, Message, ProcessId, Request,
-    Response,
-};
+use kinode_process_lib::{await_message, call_init, get_blob, http, println, Address, Message};
 use std::collections::HashMap;
 
-use llm_interface::openai::ChatParams;
 use llm_interface::api::openai::{spawn_openai_pkg, OpenaiApi};
+use llm_interface::openai::ChatParams;
 use llm_interface::openai::Message as OpenaiMessage;
 
 wit_bindgen::generate!({
@@ -40,7 +32,7 @@ fn default_headers() -> HashMap<String, String> {
     ])
 }
 
-fn handle_internal_messages(our: &Address) -> anyhow::Result<()> {
+fn handle_internal_messages() -> anyhow::Result<()> {
     let message = await_message()?;
 
     if !message.is_request() {
@@ -49,7 +41,7 @@ fn handle_internal_messages(our: &Address) -> anyhow::Result<()> {
     Ok(())
 }
 
-fn handle_http_messages(our: &Address, message: &Message, api: &OpenaiApi) -> Option<()> {
+fn handle_http_messages(message: &Message, api: &OpenaiApi) -> Option<()> {
     match message {
         Message::Response { .. } => {}
         Message::Request { ref body, .. } => {
@@ -64,11 +56,8 @@ fn handle_http_messages(our: &Address, message: &Message, api: &OpenaiApi) -> Op
             let body = get_blob()?;
             let bound_path = http_request.bound_path(Some(PROCESS_ID));
             match bound_path {
-                "/status" => {
-                    fetch_status(our);
-                }
                 "/send" => {
-                    send_tweet(our, &body.bytes, api);
+                    send_tweet(&body.bytes, api);
                 }
                 _ => {}
             }
@@ -77,20 +66,7 @@ fn handle_http_messages(our: &Address, message: &Message, api: &OpenaiApi) -> Op
     None
 }
 
-fn fetch_status(our: &Address) -> Option<()> {
-    let mut rng = rand::thread_rng();
-    let status = if rng.gen() { "AAA" } else { "BBB" };
-    let response = serde_json::to_string(&status).ok()?;
-    println!("sending response: {}", response);
-    let _ = http::send_response(
-        http::StatusCode::OK,
-        Some(default_headers()),
-        response.as_bytes().to_vec(),
-    );
-    None
-}
-
-fn send_tweet(our: &Address, body: &[u8], api: &OpenaiApi) -> Option<()> {
+fn send_tweet(body: &[u8], api: &OpenaiApi) -> Option<()> {
     let tweets: Vec<String> =
         match serde_json::from_slice::<serde_json::Value>(body).ok()?["tweets"].as_array() {
             Some(tweets) => tweets
@@ -99,7 +75,7 @@ fn send_tweet(our: &Address, body: &[u8], api: &OpenaiApi) -> Option<()> {
                 .collect(),
             None => vec![],
         };
-    let tweet_results = make_request(our, &tweets, api).ok()?;
+    let tweet_results = make_request(&tweets, api).ok()?;
     // assert_eq!(tweets.len(), tweet_results.len(), "Tweets and results length mismatch");
 
     let response_body =
@@ -113,7 +89,7 @@ fn send_tweet(our: &Address, body: &[u8], api: &OpenaiApi) -> Option<()> {
     None
 }
 
-fn make_request(our: &Address, tweets: &[String], api: &OpenaiApi) -> anyhow::Result<Vec<bool>> {
+fn make_request(tweets: &[String], api: &OpenaiApi) -> anyhow::Result<Vec<bool>> {
     let temp_rules: Vec<String> = vec![
         "Nothing related to tech.".into(),
         "Nothing related to finance.".into(),
@@ -174,27 +150,29 @@ fn create_chat_params(messages: Vec<OpenaiMessage>) -> ChatParams {
 
 call_init!(init);
 
-fn init(our: Address) {
+fn setup(our: &Address) -> OpenaiApi {
     println!("filter: begin");
-    let _ = http::serve_index_html(&our, "ui", false, true, vec!["/", "/status", "/send"]);
+    if let Err(e) = http::serve_index_html(&our, "ui", false, true, vec!["/", "/send"]) {
+        panic!("Error serving index html: {:?}", e);
+    }
     let Ok(api) = spawn_openai_pkg(our.clone(), OPENAI_API) else {
         panic!("Failed to spawn openai pkg");
     };
+    api
+}
 
-    loop {
-        let Ok(message) = await_message() else {
-            continue;
-        };
-        println!("Message from: {}", message.source().process);
+fn init(our: Address) {
+    let api = setup(&our);
+
+    while let Ok(message) = await_message() {
         if message.source().node != our.node {
             continue;
         }
 
         if message.source().process == "http_server:distro:sys" {
-            let state = handle_http_messages(&our, &message, &api);
-            // let _ = modify_session(&our, &mut session, state);
+            handle_http_messages(&message, &api);
         } else {
-            match handle_internal_messages(&our) {
+            match handle_internal_messages() {
                 Ok(()) => {}
                 Err(e) => {
                     println!("auctioneer: error: {:?}", e);
